a42034f250d68a1453a380d6901a6eee1a5a0d46
Stefan Faustmann <stefan.faustmann@faustmann.de>
2025-11-12 14:02:01 +0100

    Zwischenstand

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -134,7 +134,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Faustmann UG`,
     },
     prism: {
       theme: prismThemes.github,
