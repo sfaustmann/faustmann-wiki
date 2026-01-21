e47f6bc1825769d18e7d1ae6bf4d7a79b126b3fc
Stefan Faustmann <stefan.faustmann@faustmann.de>
2026-01-21 16:36:03 +0100

    Testphase 1

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -1,6 +1,6 @@
 import lunr from "F:\\Dokumentation\\faustmann-wiki\\node_modules\\lunr\\lunr.js";
 export const removeDefaultStopWordFilter = [];
 export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=6cf8f31d";
+export const searchIndexUrl = "search-index{dir}.json?_=61de400f";
 export const searchResultLimits = 8;
 export const fuzzyMatchingDistance = 1;
